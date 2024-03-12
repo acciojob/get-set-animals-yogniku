c0dabfcc846ae806fa8045aa38aad853016ba215
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-12 17:15:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+getSpecies(){
+	return this.species
+}
+	
+  makeSound() {
+    console.log(
+The ${this.species} makes a sound
+);
+}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		 console.log("bark");
+	}
+}
+class Cat extends Animal {
+	  purr() {
+    console.log("purr");
+  }
+}
+	const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark();
 
-class Cat extends Animal {}
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr();
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
