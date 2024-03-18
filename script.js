f1b5ea347e5fa9ec804de362f07e2ac24b765757
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-18 15:04:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ class Animal {
 	constructor(species){
 		this._species=species;
 	}
-getSpecies(){
-	return this._species;
-}
-	makeSound() {
-    console.log(`The ${this.species} makes a sound`);
+ makeSound() {
+    console.log(`The ${this._species} makes a sound`);
 
 	}
-
 }
 
-class Dog extends Animal {
+class Dog extends Animal{
+constructor(species,Dog) {
+    super(species);
+    this.Animal=Dog;
+}
 	bark(){
 		 console.log("woof");
 	}
 }
+
 class Cat extends Animal {
 	  purr() {
     console.log("purr");
   }
 }
-
 
 const myCat = new Cat("Siamese");
 myCat.makeSound(); // Output: The Siamese makes a sound
